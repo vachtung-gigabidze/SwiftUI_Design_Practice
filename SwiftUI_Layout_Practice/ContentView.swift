8e0bcb88d730e00c494895f1274a813fd0bea8fb
Dmitrii Ziadik <dmitry.zyadik@gmail.com>
2025-09-19 10:29:35 +0500

    Initial Commit

---
+++ SwiftUI_Layout_Practice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI_Layout_Practice
+//
+//  Created by Dmitrii Ziadik on 19.09.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI_Layout_Practice/SwiftUI_Layout_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Layout_PracticeApp.swift
+//  SwiftUI_Layout_Practice
+//
+//  Created by Dmitrii Ziadik on 19.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Layout_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
